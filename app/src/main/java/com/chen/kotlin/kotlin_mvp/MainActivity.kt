8d0c3f030d878ef2d76efb4548aa8dac59315529
chengw32 <718339098@qq.com>
2018-01-12 17:14:04 +0800

    18/1/12

--- app/src/main/java/com/chen/kotlin/kotlin_mvp/MainActivity.kt
+++ app/src/main/java/com/chen/kotlin/kotlin_mvp/MainActivity.kt
@@ -2,10 +2,13 @@ package com.chen.kotlin.kotlin_mvp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.chen.kotlin.kotlin_mvp.Base.BaseActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
     override fun initView() {
+
     }
 
     override fun setContentView(): Int = R.layout.activity_main
@@ -13,5 +16,8 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        kt_recycle.adapter
     }
+    
 }
